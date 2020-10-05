a050b58f72fb7ae22d03e2d4ad9f74d3f1712bf9
Gargantua <martinzb949@gmail.com>
2020-10-04 18:48:25 -0700

    redid both files, used bootstrap on my HTML file and completely redid the JS, still having issues on my Number of chars slide and showing password history

--- script.js
+++ script.js
@@ -1,42 +1,107 @@
-//document.getElementById(`password`).onclick = function() { myFunction() };
+        //Working with the DOM*****************************************
+        //This is the final code
+        var genPasswordEl = document.getElementById("genPassword");
+        //This will generate the password
+        var generatePassEl = document.getElementById("generatePass");
+        //This will help users set the character count for the password
+        var charCountEl = document.getElementById("charCount");
+        //Number settings
+        var numEl = document.getElementById("num");
+        //Lower Char settings
+        var lowCaseEl = document.getElementById("lowCase");
+        //Upper Char settings
+        var upCaseEl = document.getElementById("upCase");
+        //Symbol settings
+        var symEl = document.getElementById("sym");
+        var copyEl = document.getElementById("copy");
 
-//function myFunction() {
-//var passwd = '';
-//var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-//for (i = 1; i < 9; i++) {
-//var c = Math.floor(Math.random() * chars.length + 1);
-//passwd += chars.charAt(c)
+        //Declaring constant objects
+        const randomFunc = {
+            upCase: getRandomUpperCase,
+            lowCase: getRandomLowerCase,
+            num: getRandomNumber,
+            sym: getRandomSymbol
+        };
 
-//}
-////document.getElementById("passwd").innerHTML = "";
-//console.log(passwd)
-// return passwd;
-
-//}
-
-//NEW PASS WORD GENERATOR 
-
-
-//generate
-function generate() {
-    //1-set password length/complexity with the slide bar
-    let charCount = document.getElementById("slider").value;
-
-    //2-All 4 character types included into 1 value
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
-
-    let password = "";
-
-    //3-create for loop to choose random characters, need to break this apart into 4 different 
-    for (var i = 0; i <= charCount; i++) {
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-    }
-
-    //4-prints password in password history 
-    document.getElementById("displayPW").innerHTML = password;
-
-    //5-add password to password history and places new one on top and pushes old PW down
-    document.getElementById("lastPass").innerHTML += password + "<br /> ";
+        //Generating an event
+        generatePassEl.addEventListener('click', () => {
+            const charCount = +charCountEl.value;
+            const upCaseCheck = upCaseEl.checked;
+            const lowCaseCheck = lowCaseEl.checked;
+            const numCheck = numEl.checked;
+            const symCheck = symEl.checked;
 
 
-}
+            genPasswordEl.value = generatePassword(upCaseCheck, lowCaseCheck, numCheck, symCheck, charCount);
+        });
+
+        //Pulling variables from the DOM to be used for this function 
+        function generatePassword(upCase, lowCase, num, sym, charCount) {
+            let generatedPassword = "";
+
+            const typesCount = upCase + lowCase + num + sym;
+
+            const typesArr = [{
+                upCase
+            }, {
+                lowCase
+            }, {
+                num
+            }, {
+                sym
+            }].filter(item => Object.values(item)[0]);
+
+            if (typesCount === 0) {
+                return '';
+            }
+
+            for (let i = 0; i < charCount; i += typesCount) {
+                typesArr.forEach(type => {
+                    const funcName = Object.keys(type)[0];
+                    generatedPassword += randomFunc[funcName]();
+                });
+            }
+            const finalPassword = generatedPassword.slice(0, charCount);
+            return finalPassword;
+        }
+        //My HTML check boxes 
+        //Generating random number
+        function getRandomNumber() {
+            return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+        }
+        //Generating random lowercase char
+        function getRandomLowerCase() {
+            return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+        }
+
+        //Generating random uppercase char
+        function getRandomUpperCase() {
+            return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+        }
+        //Generating random symbol
+        function getRandomSymbol() {
+            var sym = "!@#$%^&*(){}[]=<>/,.|~?";
+            return sym[Math.floor(Math.random() * sym.length)];
+        }
+
+        let me = document.getElementById("charCount").value;
+
+        var btn = document.getElementById("generatePass");
+
+        generatePass.addEventListener('click', () => {})
+
+
+        function myFunction() {
+            /* Get the text field */
+            var copyText = document.getElementById("genPassword");
+
+            //5-add password to password history and places new one on top and pushes old PW down
+            //document.getElementById("lastPass").innerHTML = myInput
+            //document.getElementById("lastPass").innerHTML += myInput + "<br /> ";
+
+            /* Copy the text inside the text field */
+            document.execCommand("copy");
+
+            /* Alert the copied text */
+            alert("Copied to Clipboard");
+        }
